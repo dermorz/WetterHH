c26454acf6b6b63ae653097124b55091b8e13506
Moritz Pein <moritz.pein@gmail.com>
2011-07-19 00:45:25 +0200

    webhandlers and simple webinterface added to link to on-the-fly generated graphs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import db
+import graphs
 from parse_data import parse_data
 from pymongo.errors import DuplicateKeyError
-from tornado import ioloop
+from tornado import ioloop, web, httpserver 
 import logging
 
 def feed_db():
@@ -18,9 +19,27 @@ def feed_db():
             logging.warning('snapshot already in db: [%s]' % snapshot['timestamp'])
 
 
+class IndexRequestHandler(web.RequestHandler):
+    def get(self):
+        self.write("<a href='/gtemperature'>temperature</a><br/>\
+                    <a href='/gairpressure'>airpressure</a>")
+
+class GraphRequestHandler(web.RequestHandler):
+    def get(self, graph_type):
+        if graph_type not in ['temperature', 'airpressure']:
+            raise web.HTTPError(404)
+        self.set_header("Content-Type", "image/png")
+        self.write(graphs.graph(graph_type))
+
 if __name__ == '__main__':
     # initial db-feed before timer starts
-    feed_db()
+    #feed_db()
+    application = web.Application([
+        (r"/", IndexRequestHandler),
+        (r"/g(.*)", GraphRequestHandler),
+    ])
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(8080)
     io_loop = ioloop.IOLoop.instance()
     feed_db_scheduler = ioloop.PeriodicCallback(feed_db, 1000 * 60 * 5, io_loop=io_loop)
     feed_db_scheduler.start()
