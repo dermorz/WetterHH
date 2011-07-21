31df1f10a2045eb8711ad35f09080cc0c195af36
Moritz Pein <moritz.pein@gmail.com>
2011-07-22 01:05:47 +0200

    dropped mongokit for simplicity(!)

--- db.py
+++ db.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
-from mongokit import *
+# -*- coding: utf-8 -*-
+
+"""\
+snapshot = {'temperature': float,
+            'humidity': int,
+            'airpressure': float,
+            'precipitation': float,
+            'irradiance': int,
+            'windspeed': float,
+            'winddirection': int,
+            'timestamp': datetime.datetime}\
+"""
+
+import pymongo
 from datetime import datetime
 
-connection = Connection()
+connection = pymongo.Connection()
+database = connection.weatherdata
+collection = database.snapshots
+collection.ensure_index('timestamp', unique=True)
 
-@connection.register
-class Snapshot(Document):
-    __collection__ = 'snapshots'
-    __database__ = 'weatherdata'
-    structure = {
-        'temperature': float,
-        'humidity': int,
-        'airpressure': float,
-        'precipitation': float,
-        'irradiance': int,
-        'windspeed': float,
-        'winddirection': int,
-        'timestamp': datetime
-    }
-    indexes = [
-        {
-            'fields': 'timestamp',
-            'unique': True
-        }
-    ]
+def save(snapshot):
+    collection.save(snapshot)
+    connection.end_request()
 
+def get_last_n_snapshots(data_type, n):
+    return collection.find({}, {data_type: 1}).sort('timestamp', -1).limit(n)
 
+# following just for readability
+def get_last_hour(data_type):
+    return get_last_n_snapshots(data_type, 60)
+
+def get_last_6_hours(data_type):
+    return get_last_n_snapshots(data_type, 60*6)
+
+def get_last_day(data_type):
+    return get_last_n_snapshots(data_type, 60*24)
+
+def get_last_3_days(data_type):
+    return get_last_n_snapshots(data_type, 60*24*3)
+
+def get_last_week(data_type):
+    return get_last_n_snapshots(data_type, 60*24*7)
