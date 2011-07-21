6b4a87629b51d91e94a08498ce888644afb0e0d4
Moritz Pein <moritz.pein@gmail.com>
2011-07-22 01:19:20 +0200

    cleanup: logging centralized, removed obsolete code because of the mongokit drop

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
-import db
+# -*- coding: utf-8 -*-
+
 import graphs
-from parse_data import parse_data
-from pymongo.errors import DuplicateKeyError
+import parser
+
 from tornado import ioloop, web, httpserver 
 import logging
 
-def feed_db():
-    data = parse_data()
-    for snapshot in data:
-        sn = db.connection.Snapshot()
-        for key in snapshot:
-            sn[key] = snapshot[key]
-        try:
-            sn.save()
-            logging.info('DB fed')
-        except DuplicateKeyError:
-            logging.warning('snapshot already in db: [%s]' % snapshot['timestamp'])
-
+logging.basicConfig()
+log = logging.getLogger("WetterHH")
+log.setLevel(logging.DEBUG)
 
 class IndexRequestHandler(web.RequestHandler):
     def get(self):
@@ -32,8 +24,6 @@ class GraphRequestHandler(web.RequestHandler):
         self.write(graphs.graph(graph_type))
 
 if __name__ == '__main__':
-    # initial db-feed before timer starts
-    #feed_db()
     application = web.Application([
         (r"/", IndexRequestHandler),
         (r"/g(.*)", GraphRequestHandler),
@@ -41,6 +31,6 @@ if __name__ == '__main__':
     http_server = httpserver.HTTPServer(application)
     http_server.listen(8080)
     io_loop = ioloop.IOLoop.instance()
-    feed_db_scheduler = ioloop.PeriodicCallback(feed_db, 1000 * 60 * 5, io_loop=io_loop)
+    feed_db_scheduler = ioloop.PeriodicCallback(parser.parse_data, 1000 * 60 * 5, io_loop=io_loop)
     feed_db_scheduler.start()
     io_loop.start()
