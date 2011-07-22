16e9c8d10cd98e4ba90cce348271b9abc9c670ff
Moritz Pein <moritz.pein@gmail.com>
2011-07-22 09:11:57 +0200

    increased the y-scale min/max to flatten the graph

--- graphs.py
+++ graphs.py
@@ -17,6 +17,8 @@ def graph(graph_type):
     values = [s[graph_type] for s in db.get_last_6_hours(graph_type)]
     values.reverse()
     plt.plot(values)
+    axis = plt.axis()
+    plt.axis([axis[0], axis[1], axis[2]-5, axis[3]+5])
     plt.ylabel(YLABELS[graph_type])
     imgdata = StringIO.StringIO()
     plt.savefig(imgdata, format='png')
